5e3e492b7284a27b3a57c24beb60df10f1822d83
Skrpion <nestor.martinez.c@uniautonoma.edu.co>
2023-08-10 10:16:41 -0500

    ci: archivos nuevos

---
+++ src/components/NavBar.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/wolf.png';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  const handleDownloadCV = () => {
+    window.location.href = "https://drive.usercontent.google.com/uc?id=15VZANPmkf8Kwi8iZdFJopf_6znnPQjtR&authuser=0&export=download";
+  };
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/nestor-mart%C3%ADnez-c-751232209/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/skrpions" target="_blank"><img src={navIcon2} alt="" /></a>
+                {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
+              </div>
+              
+              <button className="vvd" onClick={handleDownloadCV}>
+      <span>Download CV</span>
+    </button>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,103 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-avigan.png";
+import projImg2 from "../assets/img/project-skills.png";
+import projImg3 from "../assets/img/project-ecco.png";
+import projImg4 from "../assets/img/project-misak.png";
+import projImg5 from "../assets/img/project-estudio.png";
+import projImg6 from "../assets/img/project-sercor.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Avigan Cloud",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Smart Ambulance",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Ecco",
+      description: "Development & Update",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Misak Colombia",
+      description: "Implementation & Update",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Estudio Bf",
+      description: "Implementation & Update",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Sercor",
+      description: "Implementation & Update",
+      imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>
+                Each of the following projects represents my passion for creating and solving problems through technology.
+                </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="favorites">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                   {/*  <Nav.Item>
+                      <Nav.Link eventKey="favorites">Tab 1</Nav.Link>
+                    </Nav.Item> */}
+                    <Nav.Item>
+                      <Nav.Link eventKey="favorites">Favorites</Nav.Link>
+                    </Nav.Item>
+                    {/* <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item> */}
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="favorites">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
